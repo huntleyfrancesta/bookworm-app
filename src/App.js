3be6f54e2faab5e17709b11f3824e0e88890d8d2
huntleyfrancesta <huntleyfrancesta@gmail.com>
2021-06-21 14:00:10 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Search } from './Search';
+import { Results } from './Results';
+import { BackToTop } from './BackToTop';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.ref_SearchForm = React.createRef();
+		this.ref_SearchResults = React.createRef();
+	}
 
+	state = {
+		searchResults: [],
+		showBackToTop: false
+	};
+
+	// btnBackToTop = document.getElementById('back-to-top');
+
+	handleScroll = () => {
+		if (window.scrollY === 0) {
+			// back to top
+			// hide back to top button
+			// this.btnBackToTop.style.display = '';
+			this.setState({
+				showBackToTop: false
+			});
+		} else {
+			// scrolling
+			// show back to top button
+			// this.btnBackToTop.style.display = 'block';
+			this.setState({
+				showBackToTop: true
+			});
+		}
+	};
+
+	componentDidMount() {
+		window.addEventListener('scroll', this.handleScroll);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('scroll', this.handleScroll);
+	}
+
+	updateResults = data => {
+		this.setState({
+			searchResults: [data]
+		});
+		this.scrollToRef(this.ref_SearchResults);
+	};
+
+	scrollToRef = el => {
+		window.scrollTo({
+			top: el.current.offsetTop,
+			behavior: 'smooth'
+		});
+	};
+
+	render() {
+		return (
+			<>
+				<header id="head" ref={this.ref_SearchForm}>
+					<Search updateResults={data => this.updateResults(data)} />
+				</header>
+
+				<main id="main" ref={this.ref_SearchResults}>
+					{this.state.searchResults.length > 0 ? (
+						<Results searchResults={this.state.searchResults} />
+					) : null}
+				</main>
+
+				<footer>
+					{this.state.searchResults.length > 0 ? (
+						<BackToTop
+							goBackToTop={() => this.scrollToRef(this.ref_SearchForm)}
+							showBackToTop={this.state.showBackToTop}
+						/>
+					) : null}
+					<span>
+						Photo by Ria Puskas on Unsplash • Built by{' '}
+						<a
+							href="http://www.asktami.com"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Tami Williams
+						</a>
+					</span>
+				</footer>
+			</>
+		);
+	}
+}
 export default App;
